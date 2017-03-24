165b49d74c590597db5e81571c3561d515a72cab
basalbr <basal_@hotmail.com>
2017-03-24 20:02:22 -0300

    Services, notifications, observers, html and css stuff

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,10 @@
     @section('css')
         <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,700italic,400italic' rel='stylesheet'>
         <link href="https://fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="{{url(public_path('vendor/css/bootstrap.min.css'))}}"/>
         <link rel="stylesheet" type="text/css" href="{{url(public_path('css/site.css'))}}"/>
     @show
@@ -20,8 +24,8 @@
 
 </head>
 <body>
-<nav id="nav-menu">
-    <div id="nav-menu-brand"><img src="{{url(public_path('images/brand.png'))}}" /></div>
+<header id="nav-menu">
+    <div id="nav-menu-brand"><img src="{{url(public_path('images/logotipo-pequeno.png'))}}"/></div>
     <ul id="nav-menu-items">
         <li><a href="">Como funciona</a></li>
         <li><a href="">Mensalidade</a></li>
@@ -30,9 +34,33 @@
         <li><a href="">Contato</a></li>
         <li><a href="">Acessar</a></li>
     </ul>
-</nav>
-<div id="main-banner">
-    <img src="{{url(public_path('images/banner2.jpg'))}}" />
-</div>
+</header>
+<section id="main-banner">
+    <img src="{{url(public_path('images/banner.jpg'))}}"/>
+    <div id="banner-content">
+        <div id="banner-text">
+            <p class="callout">Sua contabilidade agora ficou digital<br/>Acesse nossos serviços onde você estiver</p>
+        </div>
+        <div id="banner-buttons">
+            <a href="" class="btn btn-lg btn-complete">Acesse agora mesmo</a>
+            <a href="" class="btn btn-lg btn-warning">Conheça mais sobre nós</a>
+        </div>
+    </div>
+</section>
+<section id="como-funciona">
+
+</section>
+<section id="mensalidade">
+
+</section>
+<section id="duvidas">
+
+</section>
+<section id="noticias">
+
+</section>
+<section id="contato">
+
+</section>
 </body>
 </html>
