5ab2e4b7dd563fb291ab7b5263ccbb7ba05dfcb4
basalbr <basal_@hotmail.com>
2017-03-25 14:02:05 -0300

    novos recursos landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,6 +18,7 @@
     @show
     @section('js')
         <script type="text/javascript" src="{{url(public_path('vendor/js/jquery.js'))}}"></script>
+        <script type="text/javascript" src="{{url(public_path('vendor/js/bootstrap.min.js'))}}"></script>
 @show
 
 <!-- Fonts -->
@@ -38,7 +39,7 @@
 </header>
 <section id="main-banner" class="section">
 
-    <img src="{{url(public_path('images/banner.jpg'))}}"/>
+    <img class="background-img" src="{{url(public_path('images/banner.jpg'))}}"/>
     <div class="banner-content">
         <div class="banner-text">
             <p class="callout">Sua contabilidade agora ficou digital<br/>Acesse nossos serviços onde você estiver</p>
@@ -50,7 +51,7 @@
     </div>
 </section>
 
-<section id="como-funciona" class="section bg-m">
+<section id="como-funciona" class="section">
     <div class="container">
         <div class="col-xs-12">
             <h1>Oferecemos serviços de contabilidade através da internet</h1>
@@ -102,14 +103,78 @@
         <div class="col-sm-6">
             <div class="img-holder" style="display: table; min-height: 100%; padding: 50px">
                 <img src="{{asset(public_path('images/como-funciona.png'))}}"/>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+</section>
+
+<section id="mensalidade" class="section">
+    <img class="background-img" src="{{url(public_path('images/banner-simular.jpg'))}}"/>
+    <div class="container">
+        <div class="col-xs-12">
+            <h1>Contabilidade com mensalidade acessível</h1>
+            <h3>Para saber quanto custará sua mensalidade, basta completar os campos abaixo. O valor será atualizado
+                automaticamente.</h3>
+        </div>
+        <div class="col-sm-6">
+            <form>
+                <div class='form-group'>
+                        <label for="pro_labores">Quantos sócios retiram pró-labore? <span data-trigger="hover" class="text-info"
+                                                                        title="Pró-labore é o salário dos sócios que constam no contrato social da empresa, e recolhem o INSS mensalmente para a previdência social."
+                                                                        data-toggle="tooltip" data-placement="top">(o que é isso?)</span></label>
+                        <input type='text' class='form-control numero-mask2' id='pro_labores'
+                               data-mask-placeholder='0'/>
+                    <div class="clearfix"></div>
+                </div>
+                <hr>
+                <div class='form-group'>
+                    <label for="funcionarios">Quantos funcionários possui? <span data-trigger="hover" class="text-info"
+                                                              title="Quantidade de funcionários registrados na empresa. Exigido certificado digital A1."
+                                                              data-toggle="tooltip"
+                                                              data-placement="top">(o que é isso?)</span></label>
+                    <input type='text' class='form-control numero-mask2' id='funcionarios' data-mask-placeholder='0'/>
+                </div>
+                <hr>
+                <div class='form-group'>
+                    <label for="total_documentos"> Quantos documentos fiscais são emitidos e recebidos por mês? <span data-trigger="hover"
+                                                                                               class="text-info"
+                                                                                               title="Documentos fiscais, são as notas fiscais de venda ou prestação de serviço emitidas, e as notas fiscais de aquisição de mercadorias ou serviços."
+                                                                                               data-toggle="tooltip"
+                                                                                               data-placement="top">(o que é isso?)</span></label>
+                    <input type='text' class='form-control numero-mask2' id='total_documentos'
+                           data-mask-placeholder='0'/>
+                </div>
+                <hr>
+                <div class='form-group'>
+                    <label for="total_contabeis"> Quantos documentos contábeis são emitidos por mês? <span data-trigger="hover"
+                                                                                     class="text-info"
+                                                                                     title="Neste item estão a movimentação bancária, em que cada transação corresponde a um documento contábil, assim como recibos de aluguel. Cada valor corresponderá a um documento contábil."
+                                                                                     data-toggle="tooltip"
+                                                                                     data-placement="top">(o que é isso?)</span></label>
+                    <input type='text' class='form-control numero-mask2' id='total_contabeis'
+                           data-mask-placeholder='0'/>
+                </div>
+                <hr>
+                <div class='form-group'>
+                    <label for="contabilidade">Quanto você paga hoje por mês para sua contabilidade?</label>
+                    <input type='text' class='form-control dinheiro-mask2' id='contabilidade' data-mask-placeholder='0'
+                           value="499,99"/>
+                </div>
                 <div class="clearfix"></div>
+            </form>
+        </div>
+        <div class="col-sm-6">
+            <div id="mensalidade-box">
+                <h2 class='text-center'>Sua mensalidade será</h2>
+                <div id="valor-mensalidade" class='text-center'>R$ <span>0,00</span></div>
+                <h2 class='text-center'>Você <b>economizará</b></h2>
+                <div id='valor-economia' class='text-center'>R$ <span>0,00</span></div>
+                <div class="by-year">por ano*</div>
             </div>
         </div>
         <div class="clearfix"></div>
     </div>
-    <div class="clearfix"></div>
-</section>
-<section id="mensalidade">
 
 </section>
 <section id="duvidas">
